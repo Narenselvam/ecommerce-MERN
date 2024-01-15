7a2f1e22e91f149651ba75ac26edefbdce023353
narenselvam <narenkishore75@gmail.com>
2024-01-15 10:33:52 +0000

    mongoose connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import products from "./products.js";
 import 'dotenv/config'
+import connectDB from "./config/db.js"
+
+connectDB()//DB CONNECTION
 
 const app = express()
 
@@ -10,7 +13,6 @@ app.get("/api/products", (req, res) => {
 
 })
 app.listen(process.env.PORT, () => console.log(`Server on port ${process.env.PORT}`))
-
 
 app.get("/api/products/:id", (req, res) => {
     const product = products.find((p) => p._id === req.params.id)
