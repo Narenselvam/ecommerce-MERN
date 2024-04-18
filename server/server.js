12130536173ea2b98834634a79e1a05d1a681e41
Narenselvam <126243407+Narenselvam@users.noreply.github.com>
2024-04-18 18:23:08 +0530

    pswd validation

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ connectDB()//DB CONNECTION
 
 const app = express()
 
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send("API is running...")
 })
