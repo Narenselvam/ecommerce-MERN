6b25bdaf39b7d65a130baa683b475f32b2d3a4b3
Narenselvam <126243407+Narenselvam@users.noreply.github.com>
2024-05-11 22:12:36 +0530

    build fix /

--- server/server.js
+++ server/server.js
@@ -13,13 +13,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 connectDB()//DB CONNECTION
 
 const app = express()
-app.use(cors(
-    {
-        origin: ['https://eshoppe.vercel.app'],
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    }
-))
+
 //Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -27,12 +21,23 @@ app.use(express.urlencoded({ extended: true }))
 //Cokie parser middleware
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send("API is running...")
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(pathjoin(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'))
+    })
+}
+else {
+    app.get('/', (req, res) => {
+        res.send("API is running...")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
