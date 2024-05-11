bb6a1a9b74bddadb64299e46d80b1a5a9fcc8e3a
Narenselvam <126243407+Narenselvam@users.noreply.github.com>
2024-05-11 22:28:40 +0530

    __dir to esmodule

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ import connectDB from "./config/db.js"
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
 import cors from 'cors';
-
+import { fileURLToPath } from 'url';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 connectDB()//DB CONNECTION
 
@@ -21,13 +23,13 @@ app.use(express.urlencoded({ extended: true }))
 //Cokie parser middleware
 app.use(cookieParser())
 
-
+app.use(cors());
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(pathjoin(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, '../client/build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
